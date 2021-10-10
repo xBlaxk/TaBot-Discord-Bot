2cf712e47bc05daeb8e4d54011719f65ae6a5e52
xBlaxk <63893831+xBlaxk@users.noreply.github.com>
2021-10-10 13:20:33 -0500

    Revert "Init commit"

--- Discord Bot/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "This is a ping command!",
-    execute(message, args, cmd, client, Discord) {
-        console.log(`${this.name}`)
-        message.channel.send(`>>> pong`);
-    }
-}
