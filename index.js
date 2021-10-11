58127d94268a6174881f573f33a6bbf068d19623
xBlaxk <BlaxkMaster@gmail.com>
2021-10-11 01:02:16 -0500

    test

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ handlers.forEach((handler) => {
     require(`./handlers/${handler}`)(client, Discord);
 });
 
-//TESTING
 //TESTING 2
 
 // Logs the client in, establishing a websocket connection to Discord
