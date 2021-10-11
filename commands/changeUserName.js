e0b33c57b4d77d481bb0bdeba43aa7776cf30967
xBlaxk <BlaxkMaster@gmail.com>
2021-10-10 23:36:35 -0500

    test

--- commands/changeUserName.js
+++ commands/changeUserName.js
@@ -2,6 +2,6 @@ module.export = {
     name: "changeusername",
     description: "Change users display name",
     execute(message, args, cmd, client, Discord) {
-
+        message.member.setNickname("🤖 >_");
     }
 }
